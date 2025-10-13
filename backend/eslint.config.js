06d679067f0fdaa4fc0f05069b8ee0bfdee72502
juhahinkula <hinkula@gmail.com>
2025-10-13 14:18:10 +0300

    eslint root dir settings

--- backend/eslint.config.js
+++ backend/eslint.config.js
@@ -1,6 +1,11 @@
 import js from '@eslint/js'
 import globals from 'globals'
 import tseslint from 'typescript-eslint'
+import path from 'node:path'
+import { fileURLToPath } from 'node:url'
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
 
 export default tseslint.config(
   { ignores: ['dist', 'node_modules'] },
@@ -15,6 +20,7 @@ export default tseslint.config(
       },
       parserOptions: {
         project: './tsconfig.json',
+        tsconfigRootDir: __dirname,
       },
     },
     rules: {
